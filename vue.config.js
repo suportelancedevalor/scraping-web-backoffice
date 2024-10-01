e8c357a3961e31f8090f72c5fae56bec4e09900f
V2 dev <v2.softwarehouse@gmail.com>
2024-10-01 20:39:25 -0300

    review proxy

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    proxy: {
+      '/dev/auction': {
+        target: 'https://9wf1jnv947.execute-api.us-east-1.amazonaws.com',
+        changeOrigin: true,
+        pathRewrite: { '^/dev/auction': '/dev/auction' },
+      },
+    },
+  },
+};
