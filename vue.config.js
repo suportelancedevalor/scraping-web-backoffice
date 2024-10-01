1a0be76321981d0f288201e500c815fc3400dd08
V2 dev <v2.softwarehouse@gmail.com>
2024-10-01 20:37:11 -0300

    review cors

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-
-module.exports = {
-  devServer: {
-    proxy: {
-      '/dev/auction': {
-        target: 'https://9wf1jnv947.execute-api.us-east-1.amazonaws.com',
-        changeOrigin: true,
-        pathRewrite: { '^/home': '/home' },
-      },
-    },
-  },
-};
