76482807dd66a5b30347475330d3914d9dd8f631
V2 dev <v2.softwarehouse@gmail.com>
2024-10-01 20:30:13 -0300

    review cors

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
       '/dev/auction': {
         target: 'https://9wf1jnv947.execute-api.us-east-1.amazonaws.com',
         changeOrigin: true,
-        pathRewrite: { '^/dev/auction': '/dev/auction' },
+        pathRewrite: { '^/home': '/home' },
       },
     },
   },
